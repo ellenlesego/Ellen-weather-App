68a25dedaef79be640a0a294ed7b4d3fed3843e2
ellenlesego <lsgellen@gmail.com>
2023-12-23 22:18:38 +0200

    updating

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+function refreshweather (response){
+    let temperatureelement = document.querySelector ("#temperature");
+    let temperature = response.data.temperature.current;
+    temperatureelement.innerHTML = math.round (temperature);
+   
+}
+
+function searchcity (city){
+    let api = "71f9ca0043bt0dd90932oabfad0d3cb6";
+    let apiurl = 'https://api.shecodes.io/weather/v1/current?query${city}&key=${apikey}&units=metric';
+   axios,get (apiurl).then (refreshweather);
+   
+}
+
 function handlesearchsubmit(event){
     event.preventdefault();
-    let searchinput = document.querySelector("search-form-input")
-    let cityelement = document.querySelector("#city")
+    let searchinput = document.querySelector("search-form-input");
+    let cityelement = document.querySelector("#city");
     cityelement.innerHTML = searchinput.value;
-    
+    searchcity (searchinput.value);
+
 }
 
 let searchformelement = document.querySelector("search-form");
 
 searchformelement.addEventListener("submit", handlesearchsubmit);
+
+
+
